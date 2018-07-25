835d782e862529161c933f1c0e008e0fcd12b5cb
Eduar Ojeda <eduar.ojeda2220@correo.policia.gov.co>
2018-07-25 11:10:05 -0500

    Actualizaciones en Startup

--- Workshop/Cuadrantes/Cuadrantes/Startup.cs
+++ Workshop/Cuadrantes/Cuadrantes/Startup.cs
@@ -33,6 +33,8 @@ namespace Cuadrantes
             //adejo
 
             // 
+
+            //
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
