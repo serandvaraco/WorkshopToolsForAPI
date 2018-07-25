1d72e842fd7b082c0b14dd1bc20a7404d012bdda
ALDEDIER MARTINEZ <aldedier.martinez2851@correo.policia.gov.co>
2018-07-25 11:51:57 -0500

    Edite controlador Seguridad metodo Registro

--- Workshop/Cuadrantes/Cuadrantes/Controllers/SeguridadController.cs
+++ Workshop/Cuadrantes/Cuadrantes/Controllers/SeguridadController.cs
@@ -22,7 +22,7 @@ namespace Cuadrantes.Controllers
         public string Registro(string cedula, DateTime fechaExpedicion,
             string telefono, string correo)
         {
-            return string.Empty;
+            return "Usuario registrado";
         }
     }
 }
