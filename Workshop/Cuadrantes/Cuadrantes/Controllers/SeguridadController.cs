7a591d1edf470709670ec96c697dba943d434c09
Eduar Ojeda <eduar.ojeda2220@correo.policia.gov.co>
2018-07-26 09:14:04 -0500

    Metodo Inicio de Sesión

--- Workshop/Cuadrantes/Cuadrantes/Controllers/SeguridadController.cs
+++ Workshop/Cuadrantes/Cuadrantes/Controllers/SeguridadController.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Cuadrantes.Model;
+using Cuadrantes.Security;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Cuadrantes.Controllers
 {
     [Route("api/[controller]")]
     public class SeguridadController : Controller
     {
+        private readonly CuadranteDataContext db;
+
+        public SeguridadController(CuadranteDataContext context)
+        {
+            db = context;
+
+        }
         /// <summary>
         /// 
         /// </summary>
@@ -20,9 +30,10 @@ namespace Cuadrantes.Controllers
         public string IniciarSesion(string usuario, string clave)
         {
 
-            InformacionUsuario User = new InformacionUsuario();
-            if (User.Nombre == usuario && User.Cedula == clave)
+            InformacionUsuario User1 = new InformacionUsuario();
+            if (User1.Nombre == usuario && User1.Cedula == clave)
             {
+                GetToken(usuario, clave);
                 return "Bienvenido !!!";
 
             }
@@ -37,6 +48,47 @@ namespace Cuadrantes.Controllers
 
                 return "Por favor Usted debe registrarse para iniciar sesión";
             }
+        }
+
+        const string key = "BeX30vkH8iy5ZMEzGG0qmw==";
+        private string GetToken(string username, string cedula)
+        {
+            var common = new Common();
+            var cedulaSHA256 = common.GenerateSHA256(cedula);
+            var user = db.InformacionUsuario.FirstOrDefault(x => x.Nombre == username && x.Cedula == cedulaSHA256);
+            if (user == null)
+                return "Credenciales no validas";
+            //Obtener los nombre de los roles autorizados por el usuario
+            //var roles = user.UsersInRoles.Where(x => x.User.UserId == user.UserId)
+            //            .Select(x => x.Role.Name).ToArray();
+            //se establece la entidad del token con un tiempo de expiración fijo de 1 minuto
+            var token = new SecurityToken
+            {
+                DisplayName = string.Concat("Mr ", user.Nombre),
+                Expiration = DateTime.Now.AddHours(1),
+                Username = user.Nombre,
+                //Roles = roles,
+                id = Guid.NewGuid()
+            };
+            #region Comments
+            //Se serializa a formato JSON (Javascript Serialization Object Notation) el token
+            /*
+             *  {
+             *      'DisplayName': 'Mr svargas',
+             *      'Expiration' : '1246843218798654', // UTC del Fecha y Hora 
+             *      'Username': 'svargas'
+             *      'Roles': [{'admin', 'sadmin', 'operador'}]
+             *  }
+             */
+            #endregion 
+            var tokenString = JsonConvert.SerializeObject(token);
+            //Se cifra y se codifica a UTF 8 el resultado obteniendo los bytes de la codificación 
+            var tokenBytes = Encoding.UTF8.GetBytes(common.Encrypt(tokenString, key));
+            //Se retorna la codificación a Base64 para su transporte por HTTP
+            return Convert.ToBase64String(tokenBytes);
+        }
+
+
         [HttpPost]
         public string Registro(string cedula, DateTime fechaExpedicion,
             string telefono, string correo)
